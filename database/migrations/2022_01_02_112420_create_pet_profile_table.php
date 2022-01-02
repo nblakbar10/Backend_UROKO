e3a2e67c12c8c9436b3eb8bf13dd2968c582483b
andisltn48 <61143381+andisltn48@users.noreply.github.com>
2022-01-02 19:28:57 +0800

    Create 2022_01_02_112420_create_pet_profile_table.php

---
+++ database/migrations/2022_01_02_112420_create_pet_profile_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePetProfileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pet_profile', function (Blueprint $table) {
+            $table->id();
+            $table->string('pet_name');
+            $table->integer('user_id');
+            $table->string('pet_group_id');
+            $table->string('pet_species');
+            $table->string('pet_breed');
+            $table->string('pet_morph');
+            $table->string('pet_birthdate');
+            $table->string('pet_age');
+            $table->string('pet_description');
+            $table->string('pet_picture');
+            $table->string('pet_status');
+            $table->string('pet_activity_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pet_profile');
+    }
+}
