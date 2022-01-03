bdb63f5ae771c968ab8d35ee76533a567eb253a4
Nabil Akbar Pratama <nabilakbarpratama@gmail.com>
2022-01-03 13:45:10 +0800

    auction, adoption, rent, merchant

--- database/migrations/2022_01_03_023643_create_adoption_table.php
+++ database/migrations/2022_01_03_023643_create_adoption_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMerchantTable extends Migration
+class CreateAdoptionTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,8 @@ class CreateMerchantTable extends Migration
      */
     public function up()
     {
-        Schema::create('merchant', function (Blueprint $table) {
+        Schema::create('adoption', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id');
-            $table->string('user_username');
-            $table->string('merchant_name');
-            $table->string('merchant_image')->nullable();
-            $table->string('auction_item_id')->nullable();
             $table->timestamps();
         });
     }
@@ -31,6 +26,6 @@ class CreateMerchantTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('merchant');
+        Schema::dropIfExists('adoption');
     }
 }
