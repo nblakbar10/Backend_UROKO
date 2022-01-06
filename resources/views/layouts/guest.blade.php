cad72c822d39c78b890751311f22c178da1eb433
andisltn48 <61143381+andisltn48@users.noreply.github.com>
2022-01-06 16:47:05 +0800

    Update PetProfile

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,24 +1,82 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.11.3/datatables.min.css" />
+    {{-- <script src="//code.jquery.com/jquery.js"></script> --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
-    </head>
-    <body>
-        <div class="font-sans text-gray-900 antialiased">
-            {{ $slot }}
-        </div>
-    </body>
+    <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.11.3/datatables.min.js"></script>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="{{ asset('template') }}/assets/vendors/mdi/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="{{ asset('template') }}/assets/vendors/css/vendor.bundle.base.css">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <link rel="stylesheet" href="{{ asset('template') }}/assets/vendors/jvectormap/jquery-jvectormap.css">
+    <link rel="stylesheet" href="{{ asset('template') }}/assets/vendors/flag-icon-css/css/flag-icon.min.css">
+    <link rel="stylesheet" href="{{ asset('template') }}/assets/vendors/owl-carousel-2/owl.carousel.min.css">
+    <link rel="stylesheet" href="{{ asset('template') }}/assets/vendors/owl-carousel-2/owl.theme.default.min.css">
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <!-- endinject -->
+    <!-- Layout styles -->
+
+    <link rel="stylesheet" href="{{ asset('template') }}/assets/vendors/select2/select2.min.css">
+    <link rel="stylesheet"
+        href="{{ asset('template') }}/assets/vendors/select2-bootstrap-theme/select2-bootstrap.min.css">
+
+    <link rel="stylesheet" href="{{ asset('template') }}/assets/css/style.css">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <!-- End layout styles -->
+    <link rel="shortcut icon" href="{{ asset('template') }}/assets/images/favicon.png" />
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</head>
+
+<body>
+    <div class="font-sans text-gray-900 antialiased">
+        {{ $slot }}
+    </div>
+</body>
+
+<script src="{{ asset('template') }}/assets/vendors/js/vendor.bundle.base.js"></script>
+<!-- endinject -->
+<!-- Plugin js for this page -->
+<script src="{{ asset('template') }}/assets/vendors/chart.js/Chart.min.js"></script>
+<script src="{{ asset('template') }}/assets/vendors/progressbar.js/progressbar.min.js"></script>
+<script src="{{ asset('template') }}/assets/vendors/jvectormap/jquery-jvectormap.min.js"></script>
+<script src="{{ asset('template') }}/assets/vendors/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+<script src="{{ asset('template') }}/assets/vendors/owl-carousel-2/owl.carousel.min.js"></script>
+<!-- End plugin js for this page -->
+<!-- inject:js -->
+<script src="{{ asset('template') }}/assets/js/off-canvas.js"></script>
+<script src="{{ asset('template') }}/assets/js/hoverable-collapse.js"></script>
+<script src="{{ asset('template') }}/assets/js/misc.js"></script>
+<script src="{{ asset('template') }}/assets/js/settings.js"></script>
+<script src="{{ asset('template') }}/assets/js/todolist.js"></script>
+<!-- endinject -->
+<!-- Custom js for this page -->
+<script src="{{ asset('template') }}/assets/js/dashboard.js"></script>
+
+<script src="{{ asset('template') }}/assets/vendors/select2/select2.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $('.js-example-basic-single').select2();
+    });
+</script>
+
 </html>
